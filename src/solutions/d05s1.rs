5bc1e58aa9f086e1bdc3b100cd5b419f8b6c5875
Bytomancer <bytomancer@gmail.com>
2025-12-05 01:02:12 -0500

    Day 5 done

--- src/solutions/d05s1.rs
+++ src/solutions/d05s1.rs
@@ -4,18 +4,46 @@ use super::solutions::input_raw;
 const DAY: u8 = 05;
 const SOL: u8 = 1;
 
-pub async fn input(example: bool) -> Vec<String> {
+pub async fn input(example: bool) -> (Vec<(u64, u64)>, Vec<u64>) {
     let raw = input_raw(DAY, example).await;
-    let lines = raw
+    let (part1, part2) = raw.split_once("\n\n").expect("no dbl ln");
+    let p1 = part1
         .lines()
-        .map(|item| item.to_owned())
         .filter(|item| item.len() > 0)
+        .map(|item| {
+            let (a, b) = item.split_once("-").expect("no -");
+            let (a, b): (u64, u64) = (a.parse().expect("a invalid"), b.parse().expect("b invalid"));
+            (a, b)
+        })
         .collect();
 
-    lines
+    let p2 = part2
+        .lines()
+        .filter(|item| item.len() > 0)
+        .map(|item| item.parse::<u64>().expect("Invalid ingredient"))
+        .collect();
+
+    (p1, p2)
 }
 
 pub async fn solve(submit: bool, example: bool) {
-    let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, SOL).await;
+    let (ranges, ingredients) = input(example).await;
+    let mut ans: usize = 0;
+    for ingredient in ingredients {
+        if ing_is_fresh(ingredient, &ranges) {
+            ans += 1;
+        }
+    }
+
+    final_answer(ans, submit, DAY, SOL).await;
+}
+
+pub fn ing_is_fresh(ingredient: u64, ranges: &Vec<(u64, u64)>) -> bool {
+    for range in ranges {
+        let (l, r) = range;
+        if ingredient >= *l && ingredient <= *r {
+            return true;
+        }
+    }
+    false
 }
--- src/solutions/d05s2.rs
+++ src/solutions/d05s2.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use super::d05s1::*;
 use super::solutions::final_answer;
 
@@ -5,6 +7,72 @@ const DAY: u8 = 05;
 const SOL: u8 = 2;
 
 pub async fn solve(submit: bool, example: bool) {
-    let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, SOL).await;
+    let (db, _) = input(example).await;
+    let db = sort_db(db);
+    let mut db = kill_useless_rows(db);
+    while let Some(merge_index) = db_needs_merge(&db) {
+        //println!("{merge_index}");
+        //println!("{db:#?}\n\n");
+        do_merge(&mut db, merge_index);
+    }
+    //println!("{db:#?}");
+    let ans = count_sizes(&db);
+    final_answer(ans, submit, DAY, SOL).await;
+}
+
+pub fn sort_db(db: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    let mut db = db;
+    db.sort_by(|a, b| a.0.cmp(&b.0));
+    db
+}
+
+pub fn kill_useless_rows(db: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    let mut new_db = vec![];
+    let mut bad_db_rows = vec![];
+    for row1 in &db {
+        for row2 in &db {
+            if row1 != row2 && row2.0 >= row1.0 && row2.1 <= row1.1 {
+                // row2 is fully contained in row1!
+                bad_db_rows.push(row2.clone());
+            }
+        }
+    }
+    for row in &db {
+        if !bad_db_rows.contains(row) {
+            new_db.push(row.clone());
+        }
+    }
+    new_db
+}
+
+pub fn db_needs_merge(db: &Vec<(u64, u64)>) -> Option<usize> {
+    for i in 0..db.len() - 1 {
+        let (_, curr_r) = &db[i];
+        let (next_l, next_r) = &db[i + 1];
+        if curr_r >= next_l && curr_r <= next_r {
+            return Some(i);
+        }
+    }
+    None
+}
+
+pub fn do_merge(db: &mut Vec<(u64, u64)>, i: usize) {
+    let first = db.remove(i);
+    let second = db.remove(i);
+    let new = merge_two_ranges(&first, &second);
+    db.insert(i, new);
+}
+
+pub fn merge_two_ranges(r1: &(u64, u64), r2: &(u64, u64)) -> (u64, u64) {
+    let ls = [r1.0, r2.0];
+    let rs = [r1.1, r2.1];
+    (*ls.iter().min().unwrap(), *rs.iter().max().unwrap())
+}
+
+pub fn count_sizes(db: &Vec<(u64, u64)>) -> u64 {
+    let mut ans = 0;
+    for row in db {
+        ans += row.1 - row.0 + 1;
+    }
+    ans
 }
