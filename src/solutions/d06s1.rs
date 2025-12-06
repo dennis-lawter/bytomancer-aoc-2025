59cfc09d0abebf680a99c14665bd89b80b1c9fae
Bytomancer <bytomancer@gmail.com>
2025-12-06 01:07:28 -0500

    Day 6

--- src/solutions/d06s1.rs
+++ src/solutions/d06s1.rs
@@ -1,21 +1,66 @@
+#![allow(warnings)]
+
 use super::solutions::final_answer;
 use super::solutions::input_raw;
+use std::collections::VecDeque;
 
 const DAY: u8 = 06;
 const SOL: u8 = 1;
 
-pub async fn input(example: bool) -> Vec<String> {
-    let raw = input_raw(DAY, example).await;
-    let lines = raw
+pub async fn input(example: bool) -> (Vec<Vec<u64>>, Vec<char>) {
+    let mut raw = input_raw(DAY, example).await;
+    while raw.contains("  ") {
+        raw = raw.replace("  ", " ");
+    }
+    let mut lines: VecDeque<String> = raw
         .lines()
-        .map(|item| item.to_owned())
         .filter(|item| item.len() > 0)
+        .map(|item| item.to_owned())
         .collect();
+    let last_line = lines.pop_back().unwrap();
+    let last_line_smaller = last_line.replace(" ", "");
+    
+    let mut numbers: Vec<Vec<u64>> = vec!();
+    while let Some(line) = lines.pop_front() {
+        numbers.push(
+            line
+                .split(" ")
+                .filter(|i| i.len() > 0)
+                .map(|i|i.parse().expect(&format!("Invalid num: {i}")))
+                .collect::<Vec<u64>>());
+    }
 
-    lines
+    let ops = last_line_smaller.chars().collect();
+
+    (numbers, ops)
 }
 
 pub async fn solve(submit: bool, example: bool) {
-    let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, SOL).await;
+    let (nums, ops) = input(example).await;
+    println!("{ops:#?}");
+    let mut col_ans: Vec<u64> = vec!();
+    for x in 0..nums[0].len() {
+        let mut col = 0u64;
+        for y in 0..nums.len() {
+            match ops[x] {
+                '+' => {
+                    col += nums[y][x];
+                }
+                '*' => {
+                    if y == 0 {
+                        col = nums[y][x];
+                    } else {
+                        col *= nums[y][x];
+                    }
+                }
+                _ => {
+                    panic!("Invalid op: {}", ops[x]);
+                }
+            }
+        }
+        col_ans.push(col);
+    }
+    println!("{col_ans:#?}");
+    let ans:u64 = col_ans.iter().sum();
+    final_answer(ans, submit, DAY, SOL).await;
 }
--- src/solutions/d06s2.rs
+++ src/solutions/d06s2.rs
@@ -1,10 +1,76 @@
-use super::d06s1::*;
+#![allow(warnings)]
+
 use super::solutions::final_answer;
+use super::solutions::input_raw;
+use std::collections::VecDeque;
+
 
 const DAY: u8 = 06;
 const SOL: u8 = 2;
 
+pub async fn input(example: bool) -> (Vec<Vec<u64>>, Vec<char>) {
+    let raw = input_raw(DAY, example).await;
+    let mut lines_raw: VecDeque<String> = raw
+        .lines()
+        .filter(|i|i.len()>0)
+        .map(|i|i.to_owned())
+        .collect();
+    let last_line = lines_raw.pop_back().unwrap().replace(" ", "");
+    let ops = last_line.chars().rev().collect();
+    
+    let mut nums: Vec<Vec<u64>> = vec!();
+    let grid: Vec<Vec<char>> = lines_raw
+        .into_iter()
+        .map(|l| -> Vec<char> {l.chars().collect()})
+        .collect();
+
+    let mut n_group: Vec<u64> = vec!();
+    for x in (0..grid[0].len()).rev() {
+        let mut num = 0;
+
+        for y in (0..grid.len()) {
+            if grid[y][x] != ' ' {
+                num *= 10;
+                num += grid[y][x].to_digit(10).expect("Invalid char...") as u64;
+            }
+        }
+        println!("{num}");
+        if num == 0 {
+            nums.push(n_group);
+            n_group = vec!();
+        } else {
+            n_group.push(num);
+        }
+    }
+    nums.push(n_group);
+
+    (nums, ops)
+}
+
 pub async fn solve(submit: bool, example: bool) {
-    let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, SOL).await;
+    let (nums, ops) = input(example).await;
+    println!("{nums:#?}\n\n\n");
+    let mut col_ans: Vec<u64> = vec!();
+    for (i, group) in nums.iter().enumerate() {
+        let op = ops[i];
+        let col = match op {
+            '*' => group.iter().product(),
+            '+' => group.iter().sum(),
+            _ => panic!("Invalid op"),
+        };
+        for (j, n) in group.iter().enumerate() {
+            if j!=0 {
+                print!("{op}")
+            }
+            print!("{n}");
+        }
+        println!(" = {col}");
+        col_ans.push(col);
+    }
+    // println!("{col_ans:#?}");
+    let ans:u64 = col_ans.iter().sum();
+
+    
+
+    final_answer(ans, submit, DAY, SOL).await;
 }
